be445c7ceb307ca0b7571259c5236b8bd97fb379
abstract829 <maxxii.1820@gmail.com>
2024-05-14 22:18:10 -0400

    add bg to chat

--- components/chat-list.tsx
+++ components/chat-list.tsx
@@ -8,7 +8,7 @@ export function ChatList({ messages }: { messages: any[] }) {
   return (
     <div className="relative mx-auto max-w-[1000px] px-4 text-white">
       {messages.map((message, index) => (
-        <div key={index} className="pb-4">
+        <div key={index} className="px-2 pt-2 pb-3 bg-appBlue2 mb-4 rounded-md">
           {message.display}
         </div>
       ))}
